fbf5219e58cf672f5a9ef3d386432dc68ab42b9b
naderalmohammadi <35940664+naderalmohammadi@users.noreply.github.com>
2018-07-21 20:11:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+document.getElementById("hours").innerHTML = hours;
+let hoursP = document.getElementById("hours");
+document.getElementById("minutes").innerHTML = minutes;
+let minutesP = document.getElementById("minutes");
+document.getElementById("seconds").innerHTML = seconds;
+let secondsP = document.getElementById("seconds");
+
+let startBtn = document.getElementById("start");
+let pauseBtn = document.getElementById("pause");
+let resetBtn = document.getElementById("reset");
+pauseBtn.disabled = true;
+
+let paused;
+
+function start() {
+    startBtn.value = 'START';
+    startBtn.disabled = true;
+    resetBtn.disabled = false;
+    pauseBtn.disabled = false;
+
+    setTimeout(function() {
+        if(paused){
+            paused = false;
+        }else{
+             paused = false;
+            if(secondsP.innerHTML == 60){
+                minutesP.innerHTML = parseInt(minutesP.innerHTML) + 1;
+                if(minutesP.innerHTML == 3){
+                    hoursP.innerHTML = parseInt(hoursP.innerHTML) + 1;
+                    minutesP.innerHTML = 0;
+                }
+                secondsP.innerHTML = 0;
+                start();
+            }else{
+                secondsP.innerHTML = parseInt(secondsP.innerHTML) + 1;
+                start();
+            }
+        }
+    }, 1000);
+}
+
+  function pause() {
+    paused = true;
+    startBtn.value = 'RESUME';
+    startBtn.disabled = false;
+    pauseBtn.disabled = true;
+    resetBtn.disabled = false;
+}
+
+function reset() {
+    startBtn.value = 'START';
+    hoursP.innerHTML = 0;
+    minutesP.innerHTML = 0;
+    secondsP.innerHTML = 0;
+
+}
